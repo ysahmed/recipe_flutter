b7704c0e078c98e787cc69756327833ce7a7c6ef
waesh <waesh.mail@gmail.com>
2024-02-11 17:11:50 +0600

    recipe detail page

--- lib/pages/recipe_detail_page.dart
+++ lib/pages/recipe_detail_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/utils/constants.dart';
+import 'package:recipe/widgets/button.dart';
+import 'package:recipe/widgets/ingredient_item.dart';
+import 'package:recipe/widgets/procedure_item.dart';
+import 'package:recipe/widgets/rating_badge.dart';
 
 class RecipeDetailPage extends StatefulWidget {
   const RecipeDetailPage({super.key});
@@ -7,9 +12,317 @@ class RecipeDetailPage extends StatefulWidget {
   State<RecipeDetailPage> createState() => _RecipeDetailPageState();
 }
 
-class _RecipeDetailPageState extends State<RecipeDetailPage> {
+class _RecipeDetailPageState extends State<RecipeDetailPage>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TabController tabController = TabController(length: 2, vsync: this);
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_horiz),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              _video(),
+              _spacer(height: 20),
+              _userDetail(),
+              _spacer(height: 10),
+              SizedBox(
+                height: 33,
+                child: TabBar(
+                  controller: tabController,
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: buttonColor,
+                  ),
+                  indicatorColor: Colors.white,
+                  labelColor: Colors.white,
+                  unselectedLabelColor: buttonColor,
+                  dividerHeight: 0,
+                  tabs: [
+                    Tab(
+                      child: Container(
+                        width: 150,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Ingredients',
+                        ),
+                      ),
+                    ),
+                    Tab(
+                      child: Container(
+                        width: 150,
+                        alignment: Alignment.center,
+                        child: Text('Procedure'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              _spacer(height: 30),
+              _itemCount(1, 10),
+              _spacer(),
+              Expanded(
+                child: TabBarView(
+                  controller: tabController,
+                  children: [
+                    ListView(
+                      children: [
+                        IngredientItem(
+                            name: 'Oil', amount: '20 ml', imageName: 'oil.jpg'),
+                        IngredientItem(
+                            name: 'Oil', amount: '20 ml', imageName: 'oil.jpg'),
+                        IngredientItem(
+                            name: 'Oil', amount: '20 ml', imageName: 'oil.jpg'),
+                        IngredientItem(
+                            name: 'Oil', amount: '20 ml', imageName: 'oil.jpg'),
+                      ],
+                    ),
+                    ListView(
+                      children: [
+                        ProcedureItem(step: 1),
+                        ProcedureItem(step: 2),
+                        ProcedureItem(step: 3),
+                        ProcedureItem(step: 4),
+                        ProcedureItem(step: 5),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _spacer({double height = 10}) {
+    return SizedBox(
+      height: height,
+    );
+  }
+
+  Widget _itemCount(int serveCount, int itemCount) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Icon(
+              Icons.coffee_maker_outlined,
+              color: Colors.black54,
+            ),
+            Text(
+              '$serveCount serve',
+              style: TextStyle(color: Colors.black54),
+            ),
+          ],
+        ),
+        Text(
+          '$itemCount Items',
+          style: TextStyle(color: Colors.black54),
+        ),
+      ],
+    );
+  }
+
+  Widget _video() {
+    return Column(
+      children: [
+        // video portion
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Stack(
+            children: [
+              SizedBox(
+                height: 150,
+                width: double.maxFinite,
+                child: Container(
+                  decoration: BoxDecoration(),
+                  child: Image.asset(
+                    'assets/images/splash_bg.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+
+              Container(
+                height: 150,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Colors.black87,
+                    Colors.transparent,
+                    Colors.transparent,
+                    Colors.black87
+                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+                ),
+              ),
+
+              // rating
+              Container(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, right: 10),
+                  child: RatingBadge('4.1'),
+                ),
+              ),
+
+              // duration + favorite icon
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 10, right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(
+                        Icons.timer_outlined,
+                        color: Colors.white,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4, right: 10),
+                        child: Text(
+                          '20 min',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: const Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Icon(
+                              Icons.bookmark_border,
+                              color: buttonColor,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // recipe name + review
+        SizedBox(
+          height: 8,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Flexible(
+              child: Text(
+                'Some dish which is very delicious',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              '(13k Review)',
+              style: TextStyle(color: Colors.black54),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _userDetail() {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              // circular Image
+              ClipRRect(
+                clipBehavior: Clip.antiAlias,
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset('assets/images/11.jpg'),
+              ),
+
+              // name & location
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Laura Wilson',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.place),
+                        Text('Lagos, Nigeria'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          //follow button
+          Button(onPressed: () {}, width: 85, height: 37, text: 'Follow'),
+        ],
+      ),
+    );
+  }
+
+  Widget _tabBar(TabController tabController) {
+    return Column(
+      children: [
+        TabBar(
+          controller: tabController,
+          indicator: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: buttonColor,
+          ),
+          indicatorColor: Colors.white,
+          tabs: [
+            Tab(
+              child: Text('Ingredients'),
+            ),
+            Tab(
+              child: Text('Procedure'),
+            ),
+          ],
+        ),
+        _itemCount(1, 10),
+      ],
+    );
+  }
+
+  Widget _tabBarView(TabController tabController) {
+    return TabBarView(children: [
+      Placeholder(),
+      Placeholder(),
+    ]);
   }
 }
--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -7,63 +7,122 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final deviceHeight = MediaQuery.of(context).size.height;
-    final deviceWidth = MediaQuery.of(context).size.width;
     return Container(
       constraints: const BoxConstraints.expand(),
       decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/splash_bg.jpg'),
-            fit: BoxFit.cover,
+        image: DecorationImage(
+          image: AssetImage('assets/images/splash_bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+        gradient: LinearGradient(colors: [Colors.black, Colors.black12]),
+      ),
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black26, Colors.black87],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-          gradient: LinearGradient(colors: [Colors.black, Colors.black12])),
-      child: SafeArea(
-        child: Scaffold(
-          extendBodyBehindAppBar: true,
-          backgroundColor: Colors.black12, // replace with gradient
-
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    SvgPicture.asset(
-                      'assets/images/app_icon.svg',
-                      height: 55,
-                      colorFilter: const ColorFilter.mode(
-                        Colors.white,
-                        BlendMode.srcIn,
-                      ),
+        ),
+        child: SafeArea(
+          child: Scaffold(
+            extendBodyBehindAppBar: true,
+            backgroundColor: Colors.black12,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 100,
+                        ),
+                        SvgPicture.asset(
+                          'assets/images/app_icon.svg',
+                          height: 80,
+                          colorFilter: const ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          '100k+ Premium Recipe',
+                          style: TextStyle(
+                            shadows: [
+                              Shadow(
+                                color: Colors.black54,
+                                offset: Offset(2, 2),
+                              ),
+                              Shadow(
+                                color: Colors.black54,
+                                offset: Offset(-0.5, -0.5),
+                              )
+                            ],
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
-                    const Text('100k+ Premium Recipe'),
-                  ],
-                ),
-                ////////
-                ///////
-                //////
-                Column(
-                  children: [
-                    // large Text
-                    Text('Get Cooking'),
-                    // small text
-                    Text('Simple Way to find Testy Recipe'),
-                    SizedBox(
-                      height: 4,
-                    ),
+                  ),
+                  ////////
+                  ///////
+                  //////
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Column(
+                          children: [
+                            // large Text
+                            Text(
+                              'Get\nCooking',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 50,
+                                height: 0.9,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
 
-                    Button(
-                      onPressed: () {},
-                      // width: deviceWidth * 0.7,
-                      // height: deviceHeight * 0.08,
-                      width: 243,
-                      height: 54,
-                      text: 'Get Started',
-                      icon: Icons.skip_next_outlined,
-                    )
-                  ],
-                )
-              ],
+                            SizedBox(
+                              height: 10,
+                            ),
+                            // small text
+                            Text(
+                              'Simple Way to find Testy Recipe',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                height: 0.9,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                          ],
+                        ),
+                        Button(
+                          onPressed: () {},
+                          // width: deviceWidth * 0.7,
+                          // height: deviceHeight * 0.08,
+                          width: 243,
+                          height: 54,
+                          text: 'Get Started',
+                          icon: Icons.arrow_forward,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
